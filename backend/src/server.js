ed0e7837a59bbdd7efb17ac66f9cb65805ded435
Sudarshan K J <kjsudi@gmail.com>
2020-10-22 12:37:46 +0530

    Users api initial setup is completet

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { PORT, NODE_ENV } from "./config";
+import { userRoutes } from "./routes/index";
+
 const app = express();
 app.disable("x-powered-by");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const apiRouter = express.Router();
+app.use("/api", apiRouter);
+apiRouter.use("/users", userRoutes);
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
