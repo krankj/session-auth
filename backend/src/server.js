eb84a63408ea4068ece531f1cba9dead86a0492f
Sudarshan K J <kjsudi@gmail.com>
2020-10-22 12:28:22 +0530

    Server is now up and running

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,2 +1,7 @@
-// ESM syntax is supported.
-export {}
+import express from "express";
+import { PORT, NODE_ENV } from "./config";
+const app = express();
+app.disable("x-powered-by");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
