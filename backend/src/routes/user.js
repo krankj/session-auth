16bb3e7b014e5ce62fcd20a78b69912b87e841d5
Sudarshan K J <kjsudi@gmail.com>
2020-10-23 08:48:05 +0530

    Finished with implementing user sessions

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,6 +2,7 @@ import Joi from "joi";
 import express from "express";
 import User from "../models/user";
 import { signUp } from "../validations/user";
+import { parseError, sessionizeUser } from "../util/helpers";
 
 const userRoutes = express.Router();
 userRoutes.post("", async (req, res) => {
@@ -9,11 +10,13 @@ userRoutes.post("", async (req, res) => {
     const { username, email, password } = req.body;
     await Joi.assert({ username, email, password }, signUp);
     const newUser = new User({ username, email, password });
+    const sessionUser = sessionizeUser(newUser);
     await newUser.save();
-    res.send({ userId: newUser.id, username });
+    req.session.user = sessionUser;
+    res.send(sessionUser);
   } catch (err) {
     console.log(err);
-    res.status(400).send(err);
+    res.status(400).send(parseError(err));
   }
 });
 export default userRoutes;
